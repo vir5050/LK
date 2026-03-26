63a63af0ec449bfaff4a05abf1bf64395d4c09f1
vir5050 <107430924+vir5050@users.noreply.github.com>
2026-03-26 20:44:13 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+
+/**
+ * Public index file
+ */
+defined('APP_MODE') or define('APP_MODE', 'production'); // production | debug | demo | off
+defined('APP_PATH') or define('APP_PATH', dirname(__FILE__));
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
+$config = APP_PATH . '/protected/config/';
+
+require_once(APP_PATH . '/application/My.php');
+
+My::init($config)->run();
+
